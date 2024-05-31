7caf78171b97faba2d15689883c5f5112eca980a
Jerome Schwaederle <schwaederle.jerome@gmail.com>
2024-05-31 23:32:57 +0200

    snack-job in app-root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { JobDataService } from "./job-data.service";
+import { JobDataService } from "../services/job-data.service";
 import { Job } from "./models/job";
 import { HttpClientModule } from "@angular/common/http";
+import { NgForOf, NgIf } from "@angular/common";
+import { SnackJobComponent } from "./components/snack-job/snack-job.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet, HttpClientModule, NgIf, NgForOf, SnackJobComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  title = 'ng-job-search';
+  jobsAreLoaded = false;
   jobs: Job[] = [];
 
   constructor(private httpService: JobDataService) { }
 
   ngOnInit() {
     this.httpService.getAllJobs().subscribe( response => {
+      if (response.length === 0) {
+        this.jobsAreLoaded = false;
+        console.log("pas de jobs ...")
+        return;
+      }
+      this.jobsAreLoaded = true;
       this.jobs = response
       console.log("hello !!!")
       console.log(this.jobs)
