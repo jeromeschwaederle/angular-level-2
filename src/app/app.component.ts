b74887c8a7e0cc886679af1911570a66dd27d293
Jerome Schwaederle <schwaederle.jerome@gmail.com>
2024-05-31 16:45:10 +0200

    service in main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { JobDataService } from "./job-data.service";
+import { Job } from "./models/job";
+import { HttpClientModule } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-job-search';
+  jobs: Job[] = [];
+
+  constructor(private httpService: JobDataService) { }
+
+  ngOnInit() {
+    this.httpService.getAllJobs().subscribe( response => {
+      this.jobs = response
+      console.log("hello !!!")
+      console.log(this.jobs)
+    })
+  }
+
 }
